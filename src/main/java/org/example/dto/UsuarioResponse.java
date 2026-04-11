f5e86cf828fac331538f8dc9df556a5f74e19865
Jose A. Hidalgo Garcia <JAHG0002@ce.pucmm.edu.do>
2026-04-11 01:38:06 -0400

    actualizar la clase UsuarioResponse con los datos correctos

--- src/main/java/org/example/dto/UsuarioResponse.java
+++ src/main/java/org/example/dto/UsuarioResponse.java
@@ -5,9 +5,9 @@ import org.example.model.Usuario;
 import java.time.LocalDateTime;
 
 public record UsuarioResponse(
+        String id,
         String nombre,
         String email,
-        String passwordHash,
         Usuario.Rol rol,
         LocalDateTime fechaCreacion,
         Boolean activo
@@ -15,9 +15,9 @@ public record UsuarioResponse(
     public static UsuarioResponse from(Usuario usuario)
     {
         return new UsuarioResponse(
+                usuario.getId().toHexString(),
                 usuario.getNombre(),
                 usuario.getEmail(),
-                usuario.getPasswordHash(),
                 usuario.getRol(),
                 usuario.getFechaCreacion(),
                 usuario.getEstado()
